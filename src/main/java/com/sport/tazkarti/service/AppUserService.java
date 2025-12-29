d911436f5829d0577a768bafee94df2ee573bb2c
Kareem Emad <kareememad8561@gmail.com>
2025-12-29 20:09:05 +0200

    refactor: add PasswordEncoder to endoing password

--- src/main/java/com/sport/tazkarti/service/AppUserService.java
+++ src/main/java/com/sport/tazkarti/service/AppUserService.java
@@ -7,6 +7,7 @@ import com.sport.tazkarti.model.dto.RegisterRequest;
 import com.sport.tazkarti.model.dto.UserResponse;
 import com.sport.tazkarti.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,17 +15,23 @@ import org.springframework.stereotype.Service;
 public class AppUserService {
     private final AppUserRepository appUserRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse registerUser(RegisterRequest request) {
 
-        if (appUserRepository.existsByEmail(request.email())){
+        if (appUserRepository.existsByEmail(request.email())) {
             throw new DuplicateRecordException("Email already taken: " + request.email());
         }
         if (appUserRepository.existsByFanId(request.fanId())) {
             throw new DuplicateRecordException("Fan ID already exists: " + request.fanId());
         }
-        AppUser user = userMapper.toEntity(request);
-        appUserRepository.save(user);
-        return userMapper.toDtoResponse(user);
+
+        AppUser appUser = new AppUser();
+        appUser.setEmail(request.email());
+        appUser.setPassword(passwordEncoder.encode(request.password()));
+        appUser.setFanId(request.fanId());
+        appUserRepository.save(appUser);
+
+        return userMapper.toDtoResponse(appUser);
     }
 }
