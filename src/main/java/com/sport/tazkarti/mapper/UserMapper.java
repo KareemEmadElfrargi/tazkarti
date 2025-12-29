21c9aa2f0fd48296442258a1d13942f4de9e86bf
Kareem Emad <kareememad8561@gmail.com>
2025-12-29 17:42:47 +0200

    fix

---
+++ src/main/java/com/sport/tazkarti/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.sport.tazkarti.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.sport.tazkarti.model.AppUser;
+import com.sport.tazkarti.model.dto.RegisterRequest;
+import com.sport.tazkarti.model.dto.UserResponse;
+
+@Component
+public class UserMapper {
+
+    public AppUser toEntity(RegisterRequest request){
+        AppUser user = new AppUser();
+        user.setUsername(request.username());
+        user.setEmail(request.email());
+        user.setFanId(request.fanId());
+        user.setPassword(request.password());
+        return user;
+    }
+
+    public UserResponse toDtoResponse(AppUser user){
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getFanId()
+        );
+    }
+
+}
